8e6d3093709b3746a1edf18b67766857b5e9d25a
Junshan Liu <junshan_liu@esri.com>
2016-12-06 19:59:17 +0800

    more

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,50 @@
 var path = require('path');
 var webpack = require('webpack');
- 
+var cssnext = require('postcss-cssnext');
+
 module.exports = {
-  entry: './main.js',
   output: { path: __dirname, filename: 'bundle.js' },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap!postcss-loader',
+      }, {
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      }, {
+        test: /\.jsx*$/,
+        exclude: [/node_modules/, /.+\.config.js/],
+        loader: 'babel',
+      }, {
+        test: /\.(jpe?g|gif|png|svg)$/i,
+        loader: 'url-loader?limit=10000',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
     ]
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.js',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        CLIENT: JSON.stringify(true),
+        'NODE_ENV': JSON.stringify('development'),
+      }
+    }),
+  ],
 };
